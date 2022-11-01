2fdfb6e68a928caec6a09ec84b2ac82ec5baabe4
Johnny Wevany <johnny.wevany@moredev.tech>
2022-11-01 01:17:36 -0300

    starting the mobile app

---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,14 @@
+import { Center, Text } from "native-base";
+
+export function SignIn() {
+  return (
+    <Center flex={1} bgColor="gray.900">
+      <Text
+        color="white"
+        fontSize={24}
+      >
+        Open up App.js to start on your app!
+      </Text>
+    </Center>
+  )
+}
